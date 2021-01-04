ba50fed88d7b39eaf465aded34e6e74ae59ba2f3
Benjamin <Benjamin.Kroke@automation4all.de>
2021-01-04 15:03:28 +0100

    Added current US-Dollar Price via web-scraping

--- crypto_curry_toexcel.py
+++ crypto_curry_toexcel.py
@@ -1,5 +1,6 @@
 #Update daily cryptocurrencies
-from requests import Request, Session
+from requests import Request, Session, request
+import requests
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 import pickle
@@ -7,9 +8,17 @@ import pandas as pd
 import numpy as np
 import openpyxl
 import os
+from bs4 import BeautifulSoup
 
+#get current USD-Price
+url = "https://www.x-rates.com/calculator/?from=EUR&to=USD&amount=1"
+html_content= requests.get(url).text
 
+soup = BeautifulSoup(html_content, "lxml")
+usd_container = soup.findAll('span', {"class":"ccOutputRslt"})
+usd_ratio = usd_container[0].text
 
+#API Capcoinmarket
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 parameters = {
   'start':'1',
@@ -18,7 +27,7 @@ parameters = {
 }
 headers = {
   'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': '',
+  'X-CMC_PRO_API_KEY': '3559a0f0-81a8-4d39-b209-e66bdc3119e7',
 }
 
 session = Session()
@@ -32,8 +41,8 @@ except (ConnectionError, Timeout, TooManyRedirects) as e:
   print(e)
 
 #For testing i saved the Dictionary into an pickle object to save my API-Key-Credits
-with open('crypto_data1.pkl', 'wb') as crypto_dict:
-    pickle.dump(data, crypto_dict)
+# with open('crypto_data1.pkl', 'wb') as crypto_dict:
+#     pickle.dump(data, crypto_dict)
 # with open('crypto_data1.pkl', 'rb') as crypto_dict:
 #     data = pickle.load(crypto_dict)
 # crypto_dict.close()
@@ -67,11 +76,12 @@ try:
   coin_excel_df["Price in USD"] = coin_df["Price in USD"]   #read from coincapmarket the latest price and overwrite it in the dataframe from the file
   coin_excel_df["Cryptocurrency"] = coin_df["Cryptocurrency"]
   
+  
 
   writer = pd.ExcelWriter("Dashboard_Crypto.xlsx", engine="openpyxl", mode="w",index=False)
   writer.book = openpyxl.load_workbook("Dashboard_Crypto.xlsx")
   wb = openpyxl.Workbook("Dashboard_Crypto.xlsx")   #CApital Letter!!!
-  writer.sheets = dict((ws.title,ws) for ws in writer.book.worksheets) 
+  writer.sheets = dict((ws.title,ws) for ws in writer.book.worksheets)
   coin_excel_df.to_excel(writer, sheet_name="Crypto_Price",startcol = 0, index=False)
   writer.save()
   writer.close()
@@ -81,7 +91,8 @@ except FileNotFoundError:
 
 wb = openpyxl.load_workbook(filename_read)
 sheet = wb["Crypto_Price"]
-
+sheet1 = wb["Crypto_Omni"]
+sheet1["I2"] = usd_ratio
 sheet.column_dimensions["A"].width = 20
 sheet.column_dimensions["B"].width = 20
 sheet.column_dimensions["C"].width = 20
